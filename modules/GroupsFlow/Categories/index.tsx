e8311ea9f7891c1e272564b3f2f6a17bb2fbdbfb
oscarfarias <microcybort@gmail.com>
2024-06-11 05:40:50 -0400

    feat: :art: groups flow

--- modules/GroupsFlow/Categories/index.tsx
+++ modules/GroupsFlow/Categories/index.tsx
@@ -6,55 +6,91 @@ import Tab from '@mui/material/Tab'
 import TabContext from '@mui/lab/TabContext'
 import TabList from '@mui/lab/TabList'
 import TabPanel from '@mui/lab/TabPanel'
+import { HeadCell } from 'common/components/TableMui'
 
-const CategoriesList = () => {
-  const [categories, setCategories] = useState(0)
+interface AthleteProps {
+  id: string
+  name: string
+  lastName: string
+  ci: string
+}
+const columns: HeadCell<AthleteProps>[] = [
+  {
+    title: `#`,
+    key: `id`,
+  },
+  {
+    title: `Nombre`,
+    key: `name`,
+    render: () => (
+      <Grid container>
+        <TextField />
+      </Grid>
+    ),
+  },
+  {
+    title: `Apellido`,
+    key: `lastName`,
+    render: () => (
+      <Grid container>
+        <TextField />
+      </Grid>
+    ),
+  },
+  {
+    title: `Cedula`,
+    key: `ci`,
+    render: () => (
+      <Grid container>
+        <TextField />
+      </Grid>
+    ),
+  },
+]
+
+const TeamList = () => {
   const [athletes, setAthletes] = useState(0)
-
-  const array = Array.from({ length: categories }, (_, i) => i + 1)
-
   const data = useMemo(() => {
     const data = Array.from({ length: athletes }, (_, i) => {
       return {
         id: `${i + 1}`,
+        name: ``,
+        lastName: ``,
+        ci: ``,
       }
     })
     return data
   }, [athletes])
+  const onAthleteChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    if (value === ``) {
+      setAthletes(0)
+    } else {
+      setAthletes(Number(value))
+    }
+  }
 
-  const columns = [
-    {
-      title: `#`,
-      key: `id`,
-    },
-    {
-      title: `Nombre`,
-      key: `name`,
-      render: () => (
-        <Grid container>
-          <TextField />
+  return (
+    <Grid container>
+      <Grid container mt={1} flexDirection="row" gap={2}>
+        <Typography mt={1}>Cantidad de atletas:</Typography>
+        <Grid item xs={5}>
+          <TextField onChange={onAthleteChange} value={athletes} />
         </Grid>
-      ),
-    },
-    {
-      title: `Apellido`,
-      key: `lastName`,
-      render: () => (
-        <Grid container>
-          <TextField />
-        </Grid>
-      ),
-    },
-    {
-      title: `Cedula`,
-      key: `ci`,
-      render: () => (
-        <Grid container>
-          <TextField />
-        </Grid>
-      ),
-    },
-  ]
+      </Grid>
+      <Grid container sx={{ width: `100%` }}>
+        <Table columns={columns} rows={data} />
+      </Grid>
+    </Grid>
+  )
+}
+
+const CategoriesList = () => {
+  const [categories, setCategories] = useState(0)
+  const [teams, setTeams] = useState(0)
+
+  const array = Array.from({ length: categories }, (_, i) => i + 1)
+  const teamArray = Array.from({ length: teams }, (_, i) => i + 1)
 
   const onTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
@@ -64,13 +100,12 @@ const CategoriesList = () => {
       setCategories(Number(value))
     }
   }
-
-  const onAthleteChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onTeamChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
     if (value === ``) {
-      setAthletes(0)
+      setTeams(0)
     } else {
-      setAthletes(Number(value))
+      setTeams(Number(value))
     }
   }
 
@@ -108,14 +143,19 @@ const CategoriesList = () => {
                     <TextField />
                   </Grid>
                 </Grid>
-                <Grid container mt={1} key={item} flexDirection="row" gap={2}>
-                  <Typography mt={1}>Cantidad de atletas:</Typography>
+                <Grid container mt={2} flexDirection="row" gap={2}>
+                  <Typography mt={1}>Numero de Equipos:</Typography>
                   <Grid item xs={5}>
-                    <TextField onChange={onAthleteChange} value={athletes} />
+                    <TextField onChange={onTeamChange} value={teams} />
                   </Grid>
                 </Grid>
-                <Grid container sx={{ width: `100%` }}>
-                  <Table columns={columns} rows={data} />
+                <Grid container mt={2} flexDirection="column" gap={2}>
+                  {teamArray.length > 0 &&
+                    teamArray.map((team) => (
+                      <Accordion key={team} title={`Equipo ${team}`}>
+                        <TeamList key={team} />
+                      </Accordion>
+                    ))}
                 </Grid>
               </Grid>
             </Accordion>
