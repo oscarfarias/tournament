a86b6b830e46f852a5444e1b571561f67a349145
oscarfarias <microcybort@gmail.com>
2024-06-17 07:10:59 -0400

    feat: :art: create categories

--- modules/Category/index.tsx
+++ modules/Category/index.tsx
@@ -1,6 +1,27 @@
 import { ReactElement } from 'react'
 import Layout from '../Layout'
 import { Grid, Typography } from '@mui/material'
+import { Button, TextField, Autocomplete } from 'common/components'
+import { Option } from 'common/types'
+const groupTypes: Option[] = [
+  {
+    label: `Grupo único`,
+    value: 0,
+  },
+  {
+    label: `Grupo 1`,
+    value: 1,
+  },
+  {
+    label: `Grupo 2`,
+    value: 2,
+  },
+  {
+    label: `Grupo 3`,
+    value: 3,
+  },
+]
+
 const Category = () => {
   return (
     <Grid container flexDirection="column">
@@ -11,6 +32,63 @@ const Category = () => {
         >
           Nueva Categoría
         </Typography>
+      </Grid>
+      <Grid container mt={6} justifyContent="center">
+        <Grid xs={6} flexDirection="column">
+          <Grid
+            container
+            sx={{
+              marginY: `5`,
+              display: `grid`,
+              gridTemplateColumns: `200px 1fr`,
+              gridRowGap: `20px`,
+            }}
+          >
+            <Grid container justifyContent="flex-end">
+              <Typography
+                sx={{
+                  fontWeight: `bold`,
+                  fontSize: `18px !important`,
+                  color: `primary.main`,
+                  mt: `6px`,
+                  mr: 3,
+                }}
+              >
+                Año de la categoría
+              </Typography>
+            </Grid>
+            <Grid item xs={8}>
+              <TextField />
+            </Grid>
+            <Grid container justifyContent="flex-end">
+              <Typography
+                sx={{
+                  fontWeight: `bold`,
+                  fontSize: `18px !important`,
+                  color: `primary.main`,
+                  mt: `6px`,
+                  mr: 3,
+                }}
+              >
+                Grupo
+              </Typography>
+            </Grid>
+            <Grid item xs={8}>
+              <Autocomplete options={groupTypes} defaultValue="" />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid container mt={3} justifyContent="center">
+        <Grid
+          item
+          sx={{
+            display: `flex`,
+            width: `320px`,
+          }}
+        >
+          <Button>Siguiente</Button>
+        </Grid>
       </Grid>
     </Grid>
   )
